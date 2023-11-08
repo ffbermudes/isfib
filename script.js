e401ab916c8dddb440f401d4e3343fa70a4dd8a6
f2lpOliveira <contato@fellipefreire.com.br>
2023-11-08 19:26:01 -0300

    Adiciona funcionalidade de envio de formulário para o Google Apps Script
    Este commit adiciona um manipulador de eventos ao formulário com a ID 'google-form'. Quando o formulário é enviado, os dados são enviados para o script do Google Apps no URL especificado ('https://script.google.com/macros/s/AKfycbzsDAbiHEWOSBltaKtXoCxVu531wrN7lMIZPxZuOKFBdTMIw2twhKain_6X2CY9EVmt/exec'). Após o envio bem-sucedido, exibe uma mensagem de sucesso no elemento com ID 'form_alerts' e limpa os campos do formulário.
    
    Alterações realizadas:
    - Adiciona manipulador de evento de envio ao formulário com a ID 'google-form'
    - Envia dados do formulário para o script do Google Apps no URL especificado usando a API fetch
    - Exibe uma mensagem de sucesso no elemento 'form_alerts' após um envio bem-sucedido
    - Limpa os campos do formulário após o envio bem-sucedido
    - Exibe uma mensagem de erro no elemento 'form_alerts' em caso de falha no envio

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const scriptURL = "";
-const form = document.forms["google-form"];
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzsDAbiHEWOSBltaKtXoCxVu531wrN7lMIZPxZuOKFBdTMIw2twhKain_6X2CY9EVmt/exec';
+const form = document.forms['google-form'];
 
-form.addEventListener("submit", e => {
+form.addEventListener('submit', e => {
   e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then(response =>
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
       $("#form_alerts").html(
-        "<div class='alert-sucess'>Mensagem enviada com sucesso</div>"
-      )
-    )
+        "<div class='alert alert-success'>Mensagem enviada com sucesso</div>"
+      );
+			form.reset();
+			
+			})
     .catch(error =>
       $("#form_alerts").html(
         "<div class='alert alert-danger'>Erro ao enviar mensagem</div>"
